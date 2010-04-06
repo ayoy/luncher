89d6a53a31314ea387b0bcf684554a62b42b51d2
Dominik Kapusta <dominik.kapusta@teleca.com>
2010-04-06 16:32:15 +0200

    Display orders per user in /orders/my, and orders for all users in index.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,19 @@
 class OrdersController < ApplicationController
-  before_filter :require_admin, :only => [:find_all_by_date, :today]
-  before_filter :require_user, :except => [:find_all_by_date, :today]
+  before_filter :require_admin, :only => [:find_all_by_date, :index]
+  before_filter :require_user, :except => [:find_all_by_date, :index]
 
   def index
+    redirect_to :action => :find_all_by_date,
+      :year => Date.current.year,
+      :month => Date.current.month,
+      :day => Date.current.day
+  end
+
+  def my
     @date = Date.current
     @orders = Order.by_user(current_user).by_date(@date).ordered_by_lunch_name
   end
-  
+
   def find_by_date
     @date = Date.civil(params[:year].to_i,
                        params[:month].to_i,
@@ -24,13 +31,6 @@ class OrdersController < ApplicationController
     @orders = Order.by_date(@date).ordered_by_lunch_name.group_by { |o| o.lunch.name }
   end
 
-  def today
-    redirect_to :action => :find_all_by_date,
-      :year => Date.current.year,
-      :month => Date.current.month,
-      :day => Date.current.day
-  end
-
   def new
     @date = Date.current
     @order = Order.new
@@ -41,14 +41,14 @@ class OrdersController < ApplicationController
     order.user_id = current_user.id
     order.total = order.lunch.price_for_user(order.user)
     flash[:notice] = "Lunch ordered!" if order.save
-    redirect_to :action => :index
+    redirect_to :action => :my
   end
 
   def destroy
     order = Order.find(params[:id])
     order.destroy
     flash[:notice] = "Order removed!"
-    redirect_to :action => :index
+    redirect_to :action => :my
   end
 
 end
